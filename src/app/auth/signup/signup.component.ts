8417ff33bc544f57ffd855db91668fa07282193b
crimson666 <ankurbsws@gmail.com>
2024-03-14 01:23:40 +0530

    Push

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -4,19 +4,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit{
-  signupForm: FormGroup | undefined;
+  signupForm!: FormGroup;
 
   constructor(){}
-  ngOnInit(): void {
-    this.signupForm =  new FormGroup({
-      userName : new FormControl('', Validators.required),
-      email : new FormControl('',Validators.required),
-      password : new FormControl('',Validators.required),
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
     });
-    
   }
 
 }
