fa6d998eda4e1675fb5935b9df7afd87e2dfe078
crimson666 <ankurbsws@gmail.com>
2024-03-26 00:18:47 +0530

    Modules

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PostModel } from '../shared/post-model';
-import { PostService } from '../shared/post.service';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +10,8 @@ export class HomeComponent implements OnInit {
 
   posts$: Array<PostModel> = [];
 
-  constructor(private postService: PostService) {
-    this.postService.getAllPost().subscribe(post => {
-      this.posts$ = post;
-    })
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
---
+++ src/app/shared/post-tile/post-tile.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { faArrowDown, faArrowUp, faComments } from '@fortawesome/free-solid-svg-icons';
+import { PostModel } from '../post-model';
+import { PostService } from '../post.service';
+
+@Component({
+  selector: 'app-post-tile',
+  templateUrl: './post-tile.component.html',
+  styleUrl: './post-tile.component.css'
+})
+export class PostTileComponent implements OnInit {
+
+  @Input()
+  posts$!: Array<PostModel>;
+
+  faArrowUp = faArrowUp;
+  faArrowDown = faArrowDown;
+  faComments = faComments;
+
+  constructor(private postService: PostService ) { 
+    this.postService.getAllPost().subscribe(post => {
+      this.posts$ = post;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
